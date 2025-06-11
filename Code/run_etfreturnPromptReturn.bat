e16860815134acac970b5fe12522fbb89836bc91
Amithsk <amithsebkanat@gmail.com>
2025-06-11 11:22:28 +0530

    Added the .bat script to run the python code in the background

---
+++ Code/run_etfreturnPromptReturn.bat
@@ -0,0 +1,10 @@
+@echo off
+set VENV_PATH=E:\ETFChatbot\etfvenv
+set SCRIPT_PATH=E:\ETFChatbot\Code\Prompt\etfreturnPromptReturn.py
+set LOG_PATH=E:\ETFChatbot\logs\etfreturnPromptReturn.log
+
+REM Ensure logs folder exists
+if not exist "E:\ETFChatbot\logs" mkdir "E:\ETFChatbot\logs"
+
+REM Run the script in background and write output to log
+start "" "%VENV_PATH%\Scripts\python.exe" "%SCRIPT_PATH%" > "%LOG_PATH%" 2>&1
