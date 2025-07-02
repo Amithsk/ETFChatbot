a5a13ca6dd78a25f88f4864bf8629a9e77e23511
Amithsk <amithsebkanat@gmail.com>
2025-07-02 14:32:58 +0530

    updated the DB retrieval data properly

--- Code/Training/train_lora_adapters.py
+++ Code/Training/train_lora_adapters.py
@@ -4,11 +4,12 @@ from utils.train_model_utils import train_metric_adapter
 from utils.train_modelDB_utils import fetch_etf_expense_ratios,fetch_etf_returns
 
 
-df_returns = fetch_etf_returns()
-pairs = generate_expense_ratio_pairs(df_returns)
-train_metric_adapter("expense_ratio", pairs)
 
 df_expense  = fetch_etf_expense_ratios()
-pairs = generate_prompt_response_return_pairs(df_expense)
+pairs = generate_expense_ratio_pairs(df_expense)
+train_metric_adapter("expense_ratio", pairs)
+
+df_returns = fetch_etf_returns()
+pairs = generate_prompt_response_return_pairs(df_returns)
 train_metric_adapter("return_ratio",pairs)
 
